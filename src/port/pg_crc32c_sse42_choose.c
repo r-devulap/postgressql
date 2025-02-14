cf80f7375f24d2fb5cd29e95dc73d4988fc6d066
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2025-02-13 21:42:31 -0800

    Add runtime dispatch for PCLMUL CRC32C implementation

--- src/port/pg_crc32c_sse42_choose.c
+++ src/port/pg_crc32c_sse42_choose.c
@@ -30,8 +30,10 @@
 
 #include "port/pg_crc32c.h"
 
+#if !defined(USE_PCLMUL_CRC32C)
+#if !defined(USE_SSE42_CRC32C)
 static bool
-pg_crc32c_sse42_pclmul_available(void)
+pg_crc32c_sse42_available(void)
 {
 	unsigned int exx[4] = {0, 0, 0, 0};
 
@@ -43,11 +45,27 @@ pg_crc32c_sse42_pclmul_available(void)
 #error cpuid instruction not available
 #endif
 
-	bool		sse42 = (exx[2] & (1 << 20)) != 0;	/* SSE 4.2 */
-	bool		pclmul = (exx[2] & (1 << 1)) != 0;	/* PCLMULQDQ */
-
-	return sse42 && pclmul;
+    return (exx[2] & (1 << 20)) != 0;	/* SSE 4.2 */
 }
+#endif // !USE_SSE42_CRC32C
+
+static bool
+pg_crc32c_pclmul_available(void)
+{
+	unsigned int exx[4] = {0, 0, 0, 0};
+
+#if defined(HAVE__GET_CPUID)
+	__get_cpuid(1, &exx[0], &exx[1], &exx[2], &exx[3]);
+#elif defined(HAVE__CPUID)
+	__cpuid(exx, 1);
+#else
+#error cpuid instruction not available
+#endif
+
+	return (exx[2] & (1 << 1)) != 0;	/* PCLMULQDQ */
+}
+
+#endif // !USE_PCLMUL_CRC32C
 
 /*
  * This gets called on the first call. It replaces the function pointer
@@ -56,12 +74,24 @@ pg_crc32c_sse42_pclmul_available(void)
 static pg_crc32c
 pg_comp_crc32c_choose(pg_crc32c crc, const void *data, size_t len)
 {
-	if (pg_crc32c_sse42_pclmul_available())
-		pg_comp_crc32c = pg_comp_crc32c_sse42;
-	else
-		pg_comp_crc32c = pg_comp_crc32c_sb8;
-
-	return pg_comp_crc32c(crc, data, len);
+#ifdef USE_PCLMUL_CRC32C
+        pg_comp_crc32c = pg_comp_crc32c_pclmul; /* SSE42 and PCLUMUL assumed*/
+#elif USE_SSE42_CRC32C
+        pg_comp_crc32c = pg_comp_crc32c_sse42; /* SSE42 assumed*/
+        if (pg_crc32c_pclmul_available())
+            pg_comp_crc32c = pg_comp_crc32c_pclmul;
+#else
+        pg_comp_crc32c = pg_comp_crc32c_sb8;
+#ifdef USE_SSE42_CRC32C_WITH_RUNTIME_CHECK
+        if (pg_crc32c_sse42_available())
+            pg_comp_crc32c = pg_comp_crc32c_sse42;
+#endif
+#ifdef USE_PCLMUL_CRC32C_WITH_RUNTIME_CHECK
+        if (pg_crc32c_pclmul_available())
+            pg_comp_crc32c = pg_comp_crc32c_pclmul;
+#endif
+#endif
+        return pg_comp_crc32c(crc, data, len);
 }
 
 PGDLLIMPORT pg_crc32c	(*pg_comp_crc32c) (pg_crc32c crc, const void *data, size_t len) = pg_comp_crc32c_choose;
